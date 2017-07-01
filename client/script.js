f4eae33d22214422b15a01eeb83b286c1b1eae13
Adhy <adhywiranatap@gmail.com>
2017-07-01 07:24:07 +0700

    execute addNewBook from client

--- client/script.js
+++ client/script.js
@@ -10,6 +10,21 @@ const fetchFromServer = (query) => {
   }).then(res => res.json());
 };
 
+const mutateFromServer = (query, variables) => {
+  return fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: {
+        input: variables
+      }
+    }),
+  }).then(res => res.json());
+};
+
 const getAllBooks = () => {
   var query = `{
     books {
@@ -62,6 +77,33 @@ const getAllMembers = () => {
   });
 };
 
+const createMember = () => {
+  var query = `mutation CreateBook($input: NewBookInput) {
+    createBook(input: $input) {
+      title,
+    }
+  }`;
+
+  var variables = {
+    title: "yess",
+    author: "joe",
+    category: "Fiction",
+  };
+
+  mutateFromServer(query, variables).then(res => {
+    var listWrapper = document.getElementById('book-list').getElementsByTagName('ul')[0];
+    var listItem = document.createElement('li');
+    listItem.innerHTML = `${res.data.createBook.title}`;
+    listWrapper.appendChild(listItem);
+  });
+}
+
 getAllBooks();
 getBooksByCategory('Fiction');
 getAllMembers();
+
+const addNewBookBtn = document.getElementById('add-new-book');
+
+addNewBookBtn.addEventListener('click', () => {
+  createMember();
+});
